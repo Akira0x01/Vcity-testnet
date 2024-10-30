73e497da5fa34dbb2671f16b9ab574a3ae8eb1d9
Akira0x01 <akira0x01@gmail.com>
2024-10-30 21:15:12 +0800

    fix

--- node/public/node_files/start-node.sh
+++ node/public/node_files/start-node.sh
@@ -141,7 +141,7 @@ if [ "$NODE_TYPE" == "snap_node" ]; then
   --keyring-backend test
 elif [ "$NODE_TYPE" == "normal_node" ]; then
   LOG_FILE="$DATA_DIR/node.log"
-  $BIN start --json-rpc.enable true --home $DATA_DIR --chain-id $CHAIN_ID --keyring-backend test >> $LOG_FILE 2>&1
+  $BIN start --json-rpc.enable true --home $DATA_DIR --chain-id $CHAIN_ID --keyring-backend test --log_level panic
 else
   $BIN start --json-rpc.enable true --home $DATA_DIR --chain-id $CHAIN_ID --keyring-backend test
 fi
